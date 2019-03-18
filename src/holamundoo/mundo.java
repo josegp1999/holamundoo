f549a796e87f7c4167c310ad1f27ee895a01c6ac
Fernando <nano9gs@hotmail.es>
2019-03-18 17:22:29 +0100

    Segundo Commit

--- src/holamundoo/mundo.java
+++ src/holamundoo/mundo.java
@@ -5,7 +5,7 @@ public class mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-
+		System.out.println("Mundo=Plano");
 	}
 
 }
